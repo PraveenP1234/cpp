565186988154d603de63b39ea9ff1244ef0feb88
PraveenP1234 <praveentnr5@gmail.com>
2022-07-16 08:05:29 +0700

    cpp code

---
+++ sample.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a,s;
+    // cout<<"enter a number";
+    // cin>>a;
+    // s=a*a;
+    // cout<<"Answer is"<<s;
+    system("cls");
+    int i,j,k,n=0;
+    for(i=0;i<5;i++)
+    {
+        for(j=i;j<4;j++)
+        {
+             cout<<" ";
+        }
+        cout<<"*";
+        for(k=0;k<i;k++)
+        {
+             cout<<" ";
+             cout<<"*";
+        }
+        cout<<"\n";
+       
+
+    }
+}
